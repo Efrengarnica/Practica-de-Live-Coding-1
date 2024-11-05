939c54f189b70d795e0d47d3aef31548c02c5bc4
Efren Garnica <efrengarnicapalacios@gmail.com>
2024-11-04 19:03:18 -0600

    Se agrega mi app para mostrar palabras de un arreglo mediante una palabra que el usuario ingresa

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// Escribe tu código aquí.
+//Traer mi lista desordenada por medio de su id, el boton y el input en donde está la palabra.
+const ulLista = document.getElementById("ulLista");
+const palabra= document.getElementById("palabra");
+const enviarBoton = document.getElementById("enviarBoton");
+
+
+//Función que recibe una Arreglo y un String.
+function imprimirAreglo(string, Arreglo){
+    let cantPalabras= string.length;
+    let nuevoArreglo=[]
+    for(let i=0;i<Arreglo.length;i++){
+        if(Arreglo[i].length>cantPalabras){
+            nuevoArreglo.push(Arreglo[i])
+        }
+    }//for
+    return nuevoArreglo;
+}// imprimirAreglo
+
+//Mi Arreglo
+const myArray = ["sol", "código", "libro", "react", "viento", "python", "git", "música", "viento", "ciudad", "nube", "mar", "viento1", "viento2", "viento3", "viento4", "viento5", "viento6", "viento7", "viento8"];
+
+//Funcion para agreggar las palabras en la lista de mi html
+function arrayPrint(arr){
+    for(let i=0;i<arr.length;i++){
+        ulLista.insertAdjacentHTML("beforeend", `
+            <li>${arr[i]}</li>
+            `)
+    }//for
+}//arrayPrint
+
+enviarBoton.addEventListener("click",function(event){
+    event.preventDefault();
+    //Borro el contenido anterior
+    while (ulLista.firstChild) {
+        ulLista.removeChild(ulLista.firstChild);
+      }
+    //Primero obtengo mi array modificado
+    let arreglo= imprimirAreglo(palabra.value,myArray);
+    //Después lo agrego a la lista
+    arrayPrint(arreglo);
+});//evento
+
+
